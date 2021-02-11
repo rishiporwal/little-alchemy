c0a6a9563cb6d43f6ab1c3553f95cdd2667dbf6c
rishiporwal <64612539+rishiporwal@users.noreply.github.com>
2021-02-10 19:46:06 -0600

    3 Done

--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+scene.setBackgroundImage(assets.image`Background`)
+let ItemNames = ["Acid Rain", "Air", "Airplane"]
+let ItemCreatedBy = [[["Rain", "Smoke"], ["Rain", "Smog"]], [[""]], [["Bird", "Steel"], ["Bird", "Metal"]]]
+let ItemImage = [assets.image`AcidRain`, assets.image`Air`, assets.image`Airplane`]
